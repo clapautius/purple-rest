a5b30352348630d283e30e8380a46830a6785b3d
Tudor M. Pristavu <clapautius@users.noreply.github.com>
2019-12-08 16:20:21 +0200

    Add basic notification when new messages arrive.

--- html-client/purple-rest.js
+++ html-client/purple-rest.js
@@ -144,6 +144,7 @@ function areThereNewMessagesP()
             if (remoteMaxId != maxCurrentId) {
                 var oldMaxId = maxCurrentId;
                 maxCurrentId = remoteMaxId;
+                notifyNewMessage("New chat messages !");
                 displayConversations(oldMaxId);
             }
         },
@@ -607,6 +608,32 @@ function onBuddyClick(name)
     });
 }
 
+
+function notifyNewMessage(msg) {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification(msg);
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification(msg);
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them any more.
+}
 
 showMainMenu();
 areThereNewMessagesP();
